f5927a9b0eb929a1b570fa97ce6bfacc24928f70
Gian518 <gian518@me.com>
2026-04-10 23:44:02 +0200

    AdonisJS Attachment + Adonis Drive

--- .adonisjs/server/routes.d.ts
+++ .adonisjs/server/routes.d.ts
@@ -4,10 +4,13 @@ type ParamValue = string | number | bigint | boolean
 
 export type ScannedRoutes = {
   ALL: {
+    'drive.fs.serve': { paramsTuple: [...ParamValue[]]; params: {'*': ParamValue[]} }
   }
   GET: {
+    'drive.fs.serve': { paramsTuple: [...ParamValue[]]; params: {'*': ParamValue[]} }
   }
   HEAD: {
+    'drive.fs.serve': { paramsTuple: [...ParamValue[]]; params: {'*': ParamValue[]} }
   }
   POST: {
   }
--- adonisrc.ts
+++ adonisrc.ts
@@ -27,7 +27,11 @@ export default defineConfig({
 | will be scanned automatically from the "./commands" directory.
 |
 	*/
-	commands: [() => import('@adonisjs/core/commands'), () => import('@adonisjs/lucid/commands')],
+	commands: [
+		() => import('@adonisjs/core/commands'),
+		() => import('@adonisjs/lucid/commands'),
+		() => import('@jrmc/adonis-attachment/commands'),
+	],
 
 	/*
 |--------------------------------------------------------------------------
@@ -56,6 +60,8 @@ export default defineConfig({
 		() => import('@adonisjs/auth/auth_provider'),
 		() => import('@adonisjs/inertia/inertia_provider'),
 		() => import('@adonisjs/i18n/i18n_provider'),
+		() => import('@jrmc/adonis-attachment/attachment_provider'),
+		() => import('@adonisjs/drive/drive_provider'),
 	],
 
 	/*
